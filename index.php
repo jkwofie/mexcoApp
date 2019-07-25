2d904232c31582ea7762abe7eebf4a3d9104c0b5
mex <judekwofie2357@gmail.com>
2019-07-25 12:00:30 -0400

    changes to header

--- index.php
+++ index.php
@@ -4,18 +4,26 @@
 	<meta charset="utf-8">
 	<meta  name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Mexco</title>
-	<link rel="stylesheet" type="text/css" href="mainstylesheet.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<div class="container">
 		<header>
 			<div class="wrapper">
-		    <img src="images/large.png"  alt="logo" id="logo" class="header" width="100" />
-		    <img src="images/dropdownmenu1.png" alt="menu" id="menu"  class="header" width="50" />
+		    <img src="images/mexco.png"  alt="logo" id="logo" class="header" width="50" />
+		    <img src="images/dropdown.png" alt="menu" id="menu"  class="header" width="50" />
 		    </div>
 		    <nav id="nav">
 		    	<ul>
-		    		<li><a href="" >Services</a></li>
+		    		<li>
+		    			<a href="#" >Services</a>
+		    		  <ul class="wrapper">
+		    			<li><a href="google.com">Web Development</a></li>
+		    			<li><a href="#">Web Development</a></li>
+		    			<li><a href="#">Web Development</a></li>
+		    			<li><a href="#">Web Development</a></li>
+		    		  </ul>
+		    		</li>
 		    		<li><a href="" >Contact</a></li>
 		    		<li><a href="" >About Us</a></li>
 		    		<li><a href="" >FAQ</a></li>
@@ -24,7 +32,6 @@
 		</header>
 	</div>
    <script type="text/javascript" src="main.js">
-   	
-   </script>
+   	</script>
 </body>
 </html>
--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 window.onload=function () {
 
      var x = document.getElementById('menu');
-    // x.onclick = function () {
+     x.onclick = function () {
+     	var click = document.getElementById('nav'); 
+     	if(click.style.display==="block"){
+     		click.style.display="none";
+     	}
+     	else{
+     	  click.style.display="block";
+     	}
+     	
+     }
  
 }
